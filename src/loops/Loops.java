54bcb359b24f40bc325b5d524c1c403984586fdc
Harkanwal Singh <hankanallt@gmail.com>
2019-11-30 10:25:13 +0100

    First Commit

---
+++ src/loops/Loops.java
@@ -0,0 +1,99 @@
+package loops;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        
+        int i=1;
+        do{
+            System.out.println("Jag kan do while loops");
+            i++;
+        }while(i<10);
+        
+        
+    }
+    }
+
+/*for each loop
+
+The for each loop is generally used with a collection
+(e. g. array). By using the for each loop, you can
+access all element's of the collection without
+knowing the number of items in*/
+
+/*do while loop
+
+The do while loop is like the while loop except that
+in do while loop, the block of code is at least
+executed one time although the condition is not
+true.
+The reason is that condition is checked at the
+bottom, not the top of the loop.*/
+
+// WHILE LOOP
+//        int i = 1;
+//        while(1<5){
+//            System.out.println("jag kan loops");
+//            i++;
+//            break;
+//        }
+//        
+//    }
+//    }
+
+//FOR LOOP
+//        int m = 10;
+//        int[] j = {11, 22, 33, 44, 55, 77, 66, 99, 88, 00, 77};
+//        System.out.println(j[9]);
+//        for (int i = 0; i < 10; i++) {
+//
+//            System.out.println("Java is Best");
+//        }
+//        for (int count = 0; count < j.length; count++) {
+//            System.out.println("Value of Count is" + count);
+//            System.out.println("array value of count is" + j[count]);
+//        }
+//    }
+//
+//}
+
+//syntax: for (start value; condition; updating)
+//The code in for loop will be executed from the start value
+//until the condition is met.
+//The updating will be made continuously to the start value
+//until the condition is met.
+
+
+//Incrementing and decrementing
+
+// i++ has been used to add 1 to i.
+//This is called incrementing.
+
+// You can decrement i by using i-- .
+
+// If you use ++i then the value is incremented before the condition is tested and
+
+// if it is i++ then i is incremented after the condition is tested.
+
+//Multiple lines of code in the For loop
+
+/*If you want the program to execute many
+lines of code in the For loop, you must put
+those lines in brackets { }.*/
+
+/*while loop
+
+The code in while loop will be
+executed while the condition is
+true.
+
+Syntax:
+
+start value;
+while (condition){
+
+statement1;
+statement2;
+...........
+updating;
+}*/
